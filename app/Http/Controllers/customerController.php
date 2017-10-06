88078c5fb7afd32cc3257beae609425faad41329
Unknown <minelukasz@gmail.com>
2017-10-06 12:10:59 +0200

    Controllers&Models
    +Controllers
    +Models

--- app/Http/Controllers/customerController.php
+++ app/Http/Controllers/customerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class costumerController extends Controller
+class customerController extends Controller
 {
     /**
      * Display a listing of the resource.
