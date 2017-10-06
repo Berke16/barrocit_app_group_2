5a6838c97b59864f396f182021ab7e733fb6638a
Unknown <d210612@edu.rocwb.nl>
2017-10-06 11:45:48 +0200

    updated Controllers
    removes wrong controllers added right controllers

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\projects;
 use Illuminate\Http\Request;
 
-class ProjectsController extends Controller
+class projectsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -41,10 +40,10 @@ class ProjectsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\projects  $projects
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(projects $projects)
+    public function show($id)
     {
         //
     }
@@ -52,10 +51,10 @@ class ProjectsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\projects  $projects
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(projects $projects)
+    public function edit($id)
     {
         //
     }
@@ -64,10 +63,10 @@ class ProjectsController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\projects  $projects
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, projects $projects)
+    public function update(Request $request, $id)
     {
         //
     }
@@ -75,10 +74,10 @@ class ProjectsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\projects  $projects
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(projects $projects)
+    public function destroy($id)
     {
         //
     }
