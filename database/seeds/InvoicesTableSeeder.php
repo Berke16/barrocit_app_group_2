3e6ebea7bba154dcb2e0a6746859a5522c7c3852
jpslimmen2 <d210612@edu.rocwb.nl>
2017-10-24 19:02:01 +0200

    W.I.P seeder 4.0

--- database/seeds/InvoicesTableSeeder.php
+++ database/seeds/InvoicesTableSeeder.php
@@ -24,9 +24,10 @@ class InvoicesTableSeeder extends Seeder
 
         foreach ($customers as $customer)
         {
-            $customer_one_count++;
-            if ($customer->projects->count() == 1)
+
+            if ($customer->projects->get(0))
             {
+                $customer_one_count++;
                 if ($customer_one_count > 5 && $customer_one_count <= 10)
                 {
                     for ($i=0; $i <24; $i++)
@@ -111,42 +112,249 @@ class InvoicesTableSeeder extends Seeder
                         ]);
                     }
                 }
+
+                if ($customer->projects->get(1))
+                {
+                    $customers_two_count++;
+                    if ($customers_two_count > 5 && $customers_two_count <= 10)
+                    {
+                        for ($i=0; $i<5;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(0)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => $faker->create()->numberBetween(100, 400),
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                    }
+                    if ($customers_two_count > 10 && $customers_two_count <= 15)
+                    {
+                        for ($i=0; $i<5;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(1)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => $faker->create()->numberBetween(100, 400),
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                    }
+                    if ($customers_two_count > 15 && $customers_two_count <= 20)
+                    {
+                        for ($i=0; $i<5;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(0)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => $faker->create()->numberBetween(100, 400),
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                        for ($i=0; $i<5;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(1)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => $faker->create()->numberBetween(100, 400),
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                    }
+                    if ($customers_two_count > 20 && $customers_two_count <= 25)
+                    {
+                        for ($i=0; $i<5;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(0)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => $faker->create()->numberBetween(100, 400),
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                            ]);
+                        }
+                        for ($i=0; $i<5;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(1)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => $faker->create()->numberBetween(100, 400),
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                            ]);
+                        }
+                    }
+                    if ($customers_two_count > 25 && $customers_two_count <= 30)
+                    {
+                        for ($i=0; $i<5;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(0)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                            ]);
+                        }
+                        for ($i=0; $i<5;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(1)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                            ]);
+                        }
+                    }
+                    if ($customers_two_count > 30 && $customers_two_count <= 35)
+                    {
+                        for ($i=0; $i<2;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(0)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                        for ($i=0; $i<3;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(0)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                            ]);
+                        }
+                        for ($i=0; $i<5;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(1)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                    }
+                    if ($customers_two_count > 35 && $customers_two_count <= 40)
+                    {
+                        for ($i=0; $i<2;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(1)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                        for ($i=0; $i<3;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(1)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                            ]);
+                        }
+                        for ($i=0; $i<5;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(0)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                    }
+                    if ($customers_two_count > 40 && $customers_two_count <= 45)
+                    {
+                        for ($i=0; $i<2;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(0)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                        for ($i=0; $i<3;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(0)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                            ]);
+                        }
+                        for ($i=0;$i<3;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(1)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                        for ($i=0;$i<2;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(1)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                            ]);
+                        }
+                    }
+                    if ($customers_two_count > 45 && $customers_two_count <= 50)
+                    {
+                        for ($i=0; $i<2;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(1)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                        for ($i=0; $i<3;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(1)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                            ]);
+                        }
+                        for ($i=0;$i<3;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(0)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                                'payday2'           => now(),
+                            ]);
+                        }
+                        for ($i=0;$i<2;$i++)
+                        {
+                            DB::table('invoices')->insert([
+                                'project_id'        => $customer->projects->get(0)->id,
+                                'description'       => $faker->create()->sentence,
+                                'price'             => 400,
+                                'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
+                            ]);
+                        }
+                    }
+                }
             }
         }
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//        foreach ($customers as $customer)
-//        {
-//            $customers_two_count++;
-//            if ($customer->projects->count() == 2)
-//            {
-//                if ($customers_two_count > 5 && $customers_two_count <= 10)
-//                {
-//                    for ($i=0; $i<1;$i++)
-//                    {
-//                        DB::table('invoices')->insert([
-//                            'project_id'        => $customer->projects->orderByRaw("RAND()")->first()->id,
-//                            'description'       => $faker->create()->sentence,
-//                            'price'             => $faker->create()->numberBetween(100, 400),
-//                            'date_of_sending'   => \Carbon\Carbon::createFromFormat('Y-m-d', '2017-11-20'),
-//                        ]);
-//                    }
-//                    for ($i=0; $i<4;$i++)
-//                    {
-//                        DB::table('invoices')->insert([
-//                            'project_id'        => $customer->projects->orderByRaw("RAND()")->first()->id,
-//                            'description'       => $faker->create()->sentence,
-//                            'price'             => $faker->create()->numberBetween(100, 400),
-//                            'payday2'           => now(),
-//                        ]);
-//                    }
-//                }
-//            }
-//        }
     }
 }
