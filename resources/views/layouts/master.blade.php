3318aae51291654c800176b8caf84b90f5ba1c22
Unknown <d210612@edu.rocwb.nl>
2017-10-31 11:07:57 +0100

    localhost version

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <!-- Styles -->
     {{--<link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">--}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="css/main.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
 </head>
 <body>
     @include('layouts.nav')
